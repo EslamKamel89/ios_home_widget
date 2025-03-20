3908abd3584abe466cc20353ecae9b2b6b83c98c
EslamKamel89 <eslam_kamel_89@hotmail.com>
2025-03-20 14:38:55 +0200

    Add initial widget implementation and assets for test_home_screen

---
+++ ios/test_home_screen/test_home_screenBundle.swift
@@ -0,0 +1,18 @@
+//
+//  test_home_screenBundle.swift
+//  test_home_screen
+//
+//  Created by eslam kamel on 20/03/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct test_home_screenBundle: WidgetBundle {
+    var body: some Widget {
+        test_home_screen()
+        test_home_screenControl()
+        test_home_screenLiveActivity()
+    }
+}
