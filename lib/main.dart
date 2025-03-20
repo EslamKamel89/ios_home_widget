a7ae8b928c2721259adf481aba5618838a727336
EslamKamel89 <eslam_kamel_89@hotmail.com>
2025-03-20 14:25:45 +0200

    Update iOS workspace to include Pods project and enhance main app configuration

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo', theme: ThemeData.dark(), home: const Homepage());
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const Homepage(),
+    );
   }
 }
